882052db921c01548f2c5ffb40d498019a2a5b09
MR <miladrostamkhan@gmail.com>
2024-02-27 23:45:06 +0330

    #2 | ایتم های لیست پروفایل

--- src/components/ProfileListPage/ProfileListItem/ProfileListItem.jsx
+++ src/components/ProfileListPage/ProfileListItem/ProfileListItem.jsx
@@ -1,6 +1,12 @@
-const ProfileListItem = ({profile}) => {
+import './ProfileListItem.css'
+
+const ProfileListItem = ({ profile }) => {
     return (
-        <h1>{profile.login}</h1>
+        <div className="profile__item">
+            <img src={profile.avatar_url} className="profile__avatar" />
+            <h3>{profile.login}</h3>
+            <button className="btn-primary mt-2">More</button>
+        </div>
     )
 }
 
