f98ae3cbba20b111dcb98a53079c732be05b2fa1
MR <miladrostamkhan@gmail.com>
2024-02-27 22:33:55 +0330

    #1 | سابمیت فرم و ارسال دیتا به پدر

--- src/components/ProfileListPage/ProfileListPage.jsx
+++ src/components/ProfileListPage/ProfileListPage.jsx
@@ -1,11 +1,23 @@
+import { useEffect } from "react"
 import ProfileList from "./ProfileList/ProfileList"
 import ProfileSearch from "./ProfileSearch/ProfileSearch"
 
 const ProfileListPage = () => {
+
+    useEffect(() => {
+        console.log(111)
+    })
+
+    const searchProfiles = ({ term }) => {
+        console.log('in root', term);
+    }
+
     return (
         <div className="container">
-            <ProfileSearch />
-            <ProfileList />
+            <main>
+                <ProfileSearch onSearch={searchProfiles} />
+                <ProfileList />
+            </main>
         </div>
     )
 }
--- src/components/ProfileListPage/ProfileSearch/ProfileSearch.jsx
+++ src/components/ProfileListPage/ProfileSearch/ProfileSearch.jsx
@@ -1,11 +1,27 @@
+import { useState } from 'react';
 import './ProfileSearch.css'
 
-const ProfileSearch = () => {
+const ProfileSearch = ({ onSearch }) => {
+
+    const [term, setTerm] = useState('');
+
+    const submitForm = (e) => {
+        e.preventDefault();
+        onSearch({ term });
+    }
+
+    const clearSearch = () => {
+        setTerm(() => {
+            onSearch({ term:'' });
+            return '';
+        });
+    }
+
     return (
-        <form className="form__sec">
-            <input type="text" placeholder="Search Users" />
-            <button className="btn-primary">Submit</button>
-            <button className="btn-ghost">Clear</button>
+        <form className="form__sec" onSubmit={submitForm}>
+            <input type="text" value={term} onChange={(e) => setTerm(e.target.value)} placeholder="Search Users" />
+            <button type="submit" className="btn-primary">Submit</button>
+            <button type="button" onClick={clearSearch} className="btn-ghost">Clear</button>
         </form>
     )
 }
